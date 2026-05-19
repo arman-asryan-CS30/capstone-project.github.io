36408389c47f5ebc56ca508bfe289d30224af7f5
arman-asryan-CS30 <7014809@spsd.sk.ca>
2026-05-19 12:18:21 -0600

    Collecting Suns
    Finished collection feature, added more content in the character bar. Started the grid.

--- sketch.js
+++ sketch.js
@@ -13,10 +13,13 @@ let button;
 //Character bar
 let sunImg;
 let sunScore = 0;
+let textX = 70
 
 //Timers
 let sunStart; let sunCurrent;
 let sunElapsed
+
+let suns = []; //Keep the suns in an array
 
 //
 let sunX; let sunY;
@@ -84,40 +87,77 @@ function charactersBar() {
   noStroke()
   image(sunImg,75,60,100,100)
   textSize(20)
-  text(sunScore,70,130)
+  textAlign(CENTER, CENTER)
+  text(sunScore,75,130)
+
+  //Pea Shooter
+  image(peaIdle[0],225,60,100,100)
+  text("150",225,130)
 }
 
-function fallSun(x,y) {
-  //while(y<height){
-    image(sunImg,x,y)
-   // y++
-  //}
+function grid() {
+  for (let x = 0; x < width ; x+=150) {
+    for (let y = 150; y < height ; y += 150) {
+      
+    }
+  }
 }
 
-let suns = []
+//Generate a sun that falls
+function fallSun() {
+    for (let s of suns) {
+      image(sunImg,s.x,s.y,60,60)
+      s.y += 5
+
+      if(s.y > height-100){
+        s.y = height
+      }
+    }
+}
+
+function collectSun() {
+  for(let s of suns){
+
+    //Check if mouseX is in the range of the width of the image
+    if (mouseX >= s.x -30 && mouseX <= s.x+30 ) {
+
+      //Chech if mouseY is in the range of the height of the image
+      if (mouseY >= s.y -30 && mouseY <= s.y+30) {
+        sunScore += 50
+        suns.splice(suns.indexOf(s),1) //Remove the sun from the screen
+      }
+    }
+  }
+}
+
 
 function draw() {
+  //Timer
   sunCurrent = millis();
   sunElapsed =sunCurrent - sunStart
-  background(220);
-  charactersBar()
-  console.log(sunElapsed)
-  if (sunElapsed > 5000) { //5 seconds, since 1s - 60 frames
-    
-    sunStart = millis();
-    suns.push({x:random(width), y:0})
-  }
-  for (let s of suns) {
-    image(sunImg,s.x,s.y,50,50)
-    s.y += 10
-  }
   
-  //if (!gameOn) {
-   // menu()
- // }
-  //else{
+  
+  background(220);
+  
 
+  
+//   if (!gameOn) {
+//    menu()
+//  }
+//   else{
+      charactersBar()
+      if (sunElapsed > 10000) { //Generate new sun every 5 seconds
+    
+        sunStart = millis();
+        suns.push({x:round(random(width)), y:0})
+      }
+      fallSun()
+    
   //}
+}
+
+function mousePressed() {
+  collectSun()
 }
 
 
